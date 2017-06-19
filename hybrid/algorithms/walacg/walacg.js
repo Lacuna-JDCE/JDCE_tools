1df5ebef15bc979ecba533df9a85b6c19ed9d9ae
Niels Groot Obbink <niels@nielsgrootobbink.com>
2017-06-19 13:50:11 +0200

    Fixed several issues with walacg algorithm adapter

--- hybrid/algorithms/walacg/walacg.js
+++ hybrid/algorithms/walacg/walacg.js
@@ -8,6 +8,7 @@ const path = require('path'),
 function run_wala(folder, html_file, timeout, callback)
 {
 	let command = 'java -jar ./algorithms/walacg/WalaCG.jar ' + folder + ' ' + html_file;
+
 	let settings = {};
 
 	if(timeout)
@@ -15,13 +16,15 @@ function run_wala(folder, html_file, timeout, callback)
 		settings = {timeout: timeout};
 	}
 
+	settings.maxBuffer = 1024 * 1000 * 1000;	// 1 GB
+
 	// Run the WALA jar.
 	let result = child_process.exec(command, settings, function(error, stdout, stderr)
 	{
 		if(error)
 		{
 			console.log('================================================================================');
-			console.log('WALA returned an error. Is the HTML file (' + html_file + ') malformed?');
+			console.log('WALA returned an error:');
 			console.log('--------------------------------------------------------------------------------');
 			console.log(error.message);
 			console.log('================================================================================');
@@ -79,6 +82,19 @@ function get_first_html_script_offset(script_data)
 }
 
 
+function fix_path(file, folder)
+{
+	let f = file.substr(folder.length);
+
+	if(f.indexOf('/') == 0)
+	{
+		return f.substr(1);
+	}else{
+		return f;
+	}
+}
+
+
 function fix_caller(caller_location, script_data, html_path, folder, first_html_script_offset)
 {
 	let script_info = get_script_by_file(caller_location.file, script_data);
@@ -93,7 +109,7 @@ function fix_caller(caller_location, script_data, html_path, folder, first_html_
 		}
 
 		// Remove the path in front of the file name, because the framework expects file names relative to the dir.
-		caller_location.file = caller_location.file.substr(folder.length + 1); // + slash
+		caller_location.file = fix_path(caller_location.file, folder);
 
 		return get_containing_function(caller_location, script_info);
 	}else{
@@ -116,7 +132,7 @@ function fix_called(called_location, script_data, html_path, folder, first_html_
 		}
 
 		// Remove the path in front of the file name, because the framework expects file names relative to the dir.
-		called_location.file = called_location.file.substr(folder.length + 1); // + slash
+		called_location.file = fix_path(called_location.file, folder);
 
 		return called_location;
 	}else{
